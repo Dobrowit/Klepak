cda60d96d590a6350bd57dbd2d993df0b0174a1b
Dobrowit <radekcz@gmail.com>
2024-08-23 15:40:10 +0200

    Update table_view.py

--- routes/table_view.py
+++ routes/table_view.py
@@ -1,12 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for
 import pandas as pd
 from utils import load_data, DATA_FILE
 
 table_view_bp = Blueprint('table_view', __name__)
 
-@table_view_bp.route('/table', methods=['GET'])
 def table_view():
     data = load_data(DATA_FILE)
     df = pd.DataFrame(data)
-    table_html = df.to_html(classes='table table-striped', index=False)
+    
+    # Funkcja do tworzenia linku HTML
+    def make_clickable(val):
+        return f'<a href="{url_for("static", filename="data/" + val)}" target="_blank">{val}</a>'
+    
+    # Zastosuj funkcję make_clickable do kolumny 'zdjecie'
+    df['zdjecie'] = df['zdjecie'].apply(make_clickable)
+    
+    # Konwertuj DataFrame na HTML z escape=False, aby zachować tagi HTML
+    table_html = df.to_html(classes='table table-striped', index=False, escape=False)
+    
     return render_template('table.html', table_html=table_html)
