bb311183a731175eefcfc5e0393eef74f0466c3f
Dobrowit <radekcz@gmail.com>
2024-08-26 23:45:52 +0200

    Item view

--- routes/item_view.py
+++ routes/item_view.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, request, render_template, url_for
 from utils import load_data, DATA_FILE, CATEGORY_FILE
 import folium
-from folium.plugins import MarkerCluster
 
 item_view_bp = Blueprint('map_view', __name__)
 
@@ -12,61 +11,37 @@ def map_view():
     kategorie = load_data(CATEGORY_FILE)
 
     if entry_id:
-        data = [entry for entry in data if entry['id'] == entry_id]
+        data = [entry for entry in data if str(entry['id']) == str(entry_id)]
         if not data:
-            return render_template('map-error.html')
+            return jsonify({'error': 'Nie znaleziono danych dla podanego ID'}), 404
     else:
         data = [{k: v for k, v in entry.items() if k != 'id'} for entry in data]
+
+    kat_id = data['kategoria']
+
+    # Znalezienie kategorii na podstawie kat_id
+    category = next((category for category in kategorie if category['id'] == kat_id), None)
+    if category:
+        nazwa_kat = category['nazwa_kat']
+        icon_symbol = category['ikona']
 
     # Utworzenie mapy
     map_ = folium.Map(location=[54.7578, 17.5610], zoom_start=15)
 
-    # Ikonka full-screen
-    folium.plugins.Fullscreen(
-        position="topright",
-        title="Expand me",
-        title_cancel="Exit me",
-        force_separate_button=True,
-    ).add_to(map_)
-
-    # Dodanie MarkerCluster do mapy
-    marker_cluster = MarkerCluster(icon_create_function="""
-        function(cluster) {
-            return L.divIcon({ 
-                html: '<div style="background-color:red;"><span>' + cluster.getChildCount() + '</span></div>', 
-                className: 'marker-cluster', 
-                iconSize: new L.Point(40, 40) 
-            });
-        }
-    """).add_to(map_)
-
-    data = load_data(DATA_FILE)
-    kategorie = load_data(CATEGORY_FILE)
-    for entry in data:
-        kat_id = entry['kategoria']
-
-        # Znalezienie kategorii na podstawie kat_id
-        category = next((category for category in kategorie if category['id'] == kat_id), None)
-        if category:
-            nazwa_kat = category['nazwa_kat']
-            icon_symbol = category['ikona']
-
-        popup_content = f"""
+    item_ = f"""
 <div>
-    <img src="{url_for('static', filename='photos/' + entry['zdjecie'])}" style="max-width:300px; max-height:200px;">
+    <img src="{url_for('static', filename='photos/' + data['zdjecie'])}" style="max-width:300px; max-height:200px;">
     <h1>{nazwa_kat}</h1>
-    <p>{entry['data']}</p>
+    <p>{data['data']}</p>
     <hr style="border: 3px solid black; margin: 0;">
-    <p>{entry['opis']}</p>
+    <p>{data['opis']}</p>
 </div>
 """
-        folium.Marker(
-            location = [entry['latitude'], entry['longitude']],
-            popup = folium.Popup(popup_content, max_width=300),
-            tooltip = nazwa_kat,
-            icon = folium.Icon(color = 'red',
-                               icon = icon_symbol,
-                               prefix = 'fa')).add_to(marker_cluster)
+    folium.Marker(
+        location = [entry['latitude'], entry['longitude']],
+        icon = folium.Icon(color = 'red',
+                            icon = icon_symbol,
+                            prefix = 'fa')).add_to(map_)
 
-    map_html = map_._repr_html_()
-    return render_template('map.html', map_html=map_html)
+    item_html = item_._repr_html_()
+    return render_template('item.html', item_html=item_html)
