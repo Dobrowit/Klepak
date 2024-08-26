5a2246e010165303b7a2b33d9699c6f87bb16ea8
Dobrowit <radekcz@gmail.com>
2024-08-27 01:55:43 +0200

    Update table_view.py

--- routes/table_view.py
+++ routes/table_view.py
@@ -1,12 +1,22 @@
 from flask import Blueprint, render_template, url_for
 import pandas as pd
-from utils import load_data, DATA_FILE
+from utils import load_data, DATA_FILE, CATEGORY_FILE
 
 table_view_bp = Blueprint('table_view', __name__)
 
 @table_view_bp.route('/table', methods=['GET'])
 def table_view():
     data = load_data(DATA_FILE)
+    categories = load_data(CATEGORY_FILE)
+    
+    # Tworzenie słownika kategorii dla szybkiego wyszukiwania
+    category_dict = {cat['id']: cat['nazwa_kat'] for cat in categories}
+
+    # Dodawanie nazwy kategorii do każdego wpisu
+    for entry in data:
+        entry['nazwa_kat'] = category_dict.get(entry.get('kategoria'), 'Nieznana kategoria')
+
+    # Konwertowanie danych na DataFrame
     df = pd.DataFrame(data)
 
     # Funkcja do tworzenia linku HTML
