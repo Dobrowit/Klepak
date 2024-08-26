1e958bd7f86a7bd71bba91fee17135d6fdb33cb3
Dobrowit <radekcz@gmail.com>
2024-08-26 23:10:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -85,9 +85,13 @@ def manifest():
 def service_worker():
     return send_from_directory('', 'service-worker.js')
 
-#@app.route('/favicon.ico')
-#def manifest():
-#    return send_from_directory('static', 'favicon.ico')
-    
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        directory=app.static_folder,
+        path='favicon.ico',
+        mimetype='image/vnd.microsoft.icon'
+    )
+
 if __name__ == '__main__':
     app.run(debug=True, port=20162)
