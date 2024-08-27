c4b724ad57fb62542f2503dbc9cf76b0efbd8404
Dobrowit <radekcz@gmail.com>
2024-08-27 02:20:12 +0200

    Update main.py

--- routes/main.py
+++ routes/main.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, url_for, redirect
-import pandas as pd
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def home():
-    return redirect(url_for('/map'))
+    return redirect(url_for('map_view.map_view'))
