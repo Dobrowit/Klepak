e2d670c425fb6cd2b6be9a1cd8644587d34e61e6
Dobrowit <radekcz@gmail.com>
2024-09-04 17:08:17 +0200

    Update TODO.ml

--- TODO.ml
+++ TODO.ml
@@ -2,3 +2,5 @@
 - rozdzielenie endpointów z API pod /api/
 - implementacja kalendarza iCAL/JSON (imprezy, śmieci)
 - wysyłanie komunikatów
+- @app.route('/', methods=['GET','POS']) if request.method == 'GET':
+- sprawdzić skrypty instalacyjne na Ubuntu, Debian i Alpine
